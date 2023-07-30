aa7f40a51230544bd4ea3b706c3b602391651be5
Om Ghumre <97293861+omg12347@users.noreply.github.com>
2023-07-30 16:21:08 +0530

    Update TaskManagement.py

--- TaskManagement.py
+++ TaskManagement.py
@@ -1,90 +1,163 @@
+import datetime
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def delete_task(self, task_index):
+        if 0 <= task_index < len(self.tasks):
+            del self.tasks[task_index]
+
+    def update_task(self, task_index, updated_task):
+        if 0 <= task_index < len(self.tasks):
+            self.tasks[task_index] = updated_task
+
+
 class Task:
-    def __init__(self, title, desc, last_date, priority):
+    def __init__(self, title, description, due_date):
         self.title = title
-        self.last_date = last_date
-        self.priority = priority
-        self.desc = desc
-        
-class TaskGenius:
-    def __init__(self):
-        self.task_list = []
+        self.description = description
+        self.due_date = due_date
 
-    def add(self, task):
-        self.task_list.append(task)
-        print("✅ Congratulations! You've successfully added a new task to your TaskGenius list!")
 
-    def delete(self, task_index):
-        if task_index < len(self.task_list):
-            del self.task_list[task_index]
-            print("🗑️ Task deleted successfully! One less thing on your plate!")
-        else:
-            print("⚠️ Oops! The task index you entered is invalid. Please recheck and try again!")
+def register():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    return User(username, password)
 
-    def update(self, task_index, updated_task):
-        if task_index < len(self.task_list):
-            self.task_list[task_index] = updated_task
-            print("🚀 Task updated successfully! You're making great progress!")
-        else:
-            print("⚠️ Oops! That task index doesn't exist. Please double-check and try again!")
-    
-    def display(self):
-        if self.task_list:
-            print("📋 Your TaskGenius List:")
-            for index, task in enumerate(self.task_list, start=1):
-                print(f"Task {index}: {task.title}")
-        else:
-            print("🤷‍♂️ Oops! No tasks found. Time to add some genius tasks!")
+def login(users):
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    return users.get(username, None) if users else None
 
-# Create an instance of TaskGenius
-task_genius = TaskGenius()
+def add_task(user):
+    title = input("Enter task title: ")
+    description = input("Enter task description: ")
+    due_date = input("Enter due date (YYYY-MM-DD): ")
 
-# Display options to the user
-menu = """
---- TaskGenius Organizer ---
-1. Add Task to list
-2. Delete Task from list
-3. Display Tasks from list 
-4. Update Task 
-5. Quit
-"""
+    try:
+        due_date = datetime.datetime.strptime(due_date, "%Y-%m-%d")
+    except ValueError:
+        print("Invalid date format. Please use YYYY-MM-DD.")
+        return
 
-while True:
-    print(menu)
-    choice = input("Enter your choice: ")
-    if choice == "1":
-        title = input("Enter task title: ")
-        desc = input("Enter task description: ")
-        last_date = input("Enter due date: ")
-        priority = input("Enter task priority: ")
-        task = Task(title, desc, last_date, priority)
-        task_genius.add(task)
+    task = Task(title, description, due_date)
+    user.add_task(task)
+    print("Task added successfully.")
 
-    elif choice == "2":
-        task_genius.display()
-        task_index = int(input("Enter the task index to delete: ")) - 1
-        if task_index < len(task_genius.task_list):
-            task_genius.delete(task_index)
-        else:
-            print("⚠️ Oops! Invalid task index. Please try again.")
+def delete_task(user):
+    if not user.tasks:
+        print("No tasks found.")
+        return
 
-    elif choice == "3":
-        task_genius.display()
-        
-    elif choice == "4":
-        task_genius.display()
-        task_index = int(input("Enter the task index to update: ")) - 1
-        if 0 <= task_index < len(task_genius.task_list):
-            title = input("Enter updated task title: ")
-            desc = input("Enter updated task description: ")
-            last_date = input("Enter updated due date: ")
-            priority = input("Enter updated task priority: ")
-            updated_task = Task(title, desc, last_date, priority)
-            task_genius.update(task_index, updated_task)
-        else:
-            print("⚠️ Oops! Invalid task index. Please try again.")
-    
-    elif choice == "5":
-        print("👋 Exiting TaskGenius... Have a productive day ahead!")
-        break
+    for index, task in enumerate(user.tasks):
+        print(f"{index + 1}. {task.title} (Due: {task.due_date})")
+
+    task_index = int(input("Enter the task number to delete: ")) - 1
+    if 0 <= task_index < len(user.tasks):
+        user.delete_task(task_index)
+        print("Task deleted successfully.")
     else:
-        print("⚠️ Invalid choice. Please try again.")
+        print("Invalid task number.")
+
+def update_task(user):
+    if not user.tasks:
+        print("No tasks found.")
+        return
+
+    for index, task in enumerate(user.tasks):
+        print(f"{index + 1}. {task.title} (Due: {task.due_date})")
+
+    task_index = int(input("Enter the task number to update: ")) - 1
+    if 0 <= task_index < len(user.tasks):
+        title = input("Enter updated task title: ")
+        description = input("Enter updated task description: ")
+        due_date = input("Enter updated due date (YYYY-MM-DD): ")
+
+        try:
+            due_date = datetime.datetime.strptime(due_date, "%Y-%m-%d")
+        except ValueError:
+            print("Invalid date format. Please use YYYY-MM-DD.")
+            return
+
+        updated_task = Task(title, description, due_date)
+        user.update_task(task_index, updated_task)
+        print("Task updated successfully.")
+    else:
+        print("Invalid task number.")
+
+def task_reminder(user):
+    today = datetime.datetime.now().date()
+    reminder_tasks = [task for task in user.tasks if task.due_date.date() == today]
+
+    print("🔔 Today's Task Reminder 🔔")
+    for task in reminder_tasks:
+        print(f"Task: {task.title} (Due: {task.due_date})")
+
+
+def main():
+    users = {}
+    user = None
+
+    while True:
+        print("\n--- DAILY TASK SCHEDULER ---")
+        print("1. Register")
+        print("2. Login")
+        print("3. Add Task")
+        print("4. Update Task")
+        print("5. Delete Task")
+        print("6. Task Reminder")
+        print("7. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            user = register()
+            users[user.username] = user
+            print("Registration successful!")
+
+        elif choice == "2":
+            user = login(users)
+            if user:
+                print(f"Welcome, {user.username}!")
+            else:
+                print("Invalid username or password. Please try again.")
+
+        elif choice == "3":
+            if user:
+                add_task(user)
+            else:
+                print("Please log in to add a task.")
+
+        elif choice == "4":
+            if user:
+                update_task(user)
+            else:
+                print("Please log in to update a task.")
+
+        elif choice == "5":
+            if user:
+                delete_task(user)
+            else:
+                print("Please log in to delete a task.")
+
+        elif choice == "6":
+            if user:
+                task_reminder(user)
+            else:
+                print("Please log in to see the task reminder.")
+
+        elif choice == "7":
+            print("Exiting the Daily Task Scheduler...")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
